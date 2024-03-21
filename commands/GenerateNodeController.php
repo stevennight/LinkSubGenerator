fba8f7c727986f8f08409789a0cd712429fbec86
Nyatori <stevennight@qq.com>
2024-03-21 23:53:04 +0800

    add: node generator change: config file

---
+++ commands/GenerateNodeController.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace app\commands;
+
+use Yii;
+use yii\console\Controller;
+
+class GenerateNodeController extends Controller
+{
+    /**
+     * 执行命令：php yii generate-node/convert
+     * @return void
+     */
+    public function actionConvert() {
+        $content = file_get_contents(Yii::getAlias('@app') . '/config/node.txt');
+        $nodes = explode(PHP_EOL, $content);
+
+        $configNodeList = Yii::$app->params['nodeList'];
+        $keyByHostPort = [];
+        foreach ($configNodeList as $node) {
+            $keyByHostPort[$node['sourceHost'] . ':' . $node['sourcePort']] = $node['forwardPort'];
+        }
+        unset($node);
+
+        $nodeSettings = [];
+        foreach ($nodes as $node) {
+            if (!preg_match(
+                '/^(?<protocol>.*?)(?<part1>:\/\/.*?@)(?<host>.*?):(?<port>.*?)(?<part2>\?.*?#)(?<label>.*?)$/',
+                $node,
+                $matches
+            )) {
+                continue;
+            }
+
+            // 名称
+            $name = $matches['label'];
+            $name = urldecode($name);
+            $name = current(explode('-', $name));
+            // link
+            $link = $matches['protocol'] . $matches['part1'] . '{host}' . ':' . '{port}' . $matches['part2'] . '{label}';
+
+            $nodeSettings[] = [
+                'name' => $name,
+                'link' => $link,
+                'protocol' => $matches['protocol'],
+                'sourceHost' => $matches['host'],
+                'sourcePort' => $matches['port'],
+                // 如果旧配置有则从旧配置获取，否则到时候再手动配置。
+                'forwardPort' => $keyByHostPort[$matches['host'] . ':' . $matches['port']] ?? 0,
+            ];
+        }
+
+        file_put_contents(Yii::getAlias('@app') . '/config/nodeList.php', '<?php return ' . var_export($nodeSettings, true) . ';');
+    }
+}
---
+++ config/params.php
@@ -0,0 +1,12 @@
+<?php
+
+return [
+    // 直连节点列表
+    'nodeList' => require __DIR__ . '/nodeList.php',
+    // 进行中转服务的节点列表
+    'relayNodeList' => require __DIR__ . '/relayNodeList.php',
+    // 中转服务列表
+    'relayList' => require __DIR__ . '/relayList.php',
+    // 转发列表
+    'forwardList' => require __DIR__ . '/forwardList.php',
+];
